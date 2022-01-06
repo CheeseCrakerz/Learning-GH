97f597eb3cc298547f824e4205354b59e2b5e66c
Jeremy Kim <jeremy@jbnbinc.com>
2022-01-06 02:28:09 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Hana Kim on 2022-01-06.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
